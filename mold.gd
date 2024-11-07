3b50c16b8cb422e98812c4a6b02532b0487dd742
Mary <mhb93@nau.edu>
2024-11-07 10:19:46 -0900

    Added electrostatic that moves toward the victim

--- mold.gd
+++ mold.gd
@@ -1,11 +1,32 @@
 extends CPUParticles2D
 
+#var your_date = get_parent()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var y_to_parent = get_position().y
+	var x_to_parent = get_position().x
+	var x_movement;
+	var y_movement;
+	
+#	Determine which direction the electrostatic is going to move
+	if(x_to_parent < 0):
+		x_movement = x_to_parent + 1;
+	else:
+		x_movement = x_to_parent - 1;
+		
+	if(y_to_parent < 0):
+		y_movement = y_to_parent + 1;
+	else:
+		y_movement = y_to_parent - 1;
+	
+#	Create the attack pattern
+	var attack_vec = Vector2(x_movement, y_movement)
+	print(x_to_parent)
+	if(y_to_parent != 0):
+		transform = Transform2D(0.0, attack_vec);
