2c87dedf50603d4feae9f60aad37ce8db3d9e939
Caleb <caleb@pop-os.localdomain>
2024-11-12 20:57:31 -0900

    Added hallway scene with gamers, made more characters, etc

--- arc_2.gd
+++ arc_2.gd
@@ -3,10 +3,20 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var dialog = Dialogic.start("pre-date-timeline")
-	add_child(dialog)
-	pass # Replace with function body.
+	_first()
+
+	#pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func _first() -> void:
+	var dialog = Dialogic.start("hallway-timeline")
+	add_child(dialog)
+	pass
+	
+func _second() -> void:
+	var dialog2 = Dialogic.start("pre-date-timeline")
+	add_child(dialog2)
 	pass
