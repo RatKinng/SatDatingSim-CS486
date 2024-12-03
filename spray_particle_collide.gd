9b6655d063177b18b0feab3a931c11e987b3cdad
Mary <mhb93@nau.edu>
2024-12-03 12:44:43 -0900

    Updated particle spacing

--- spray_particle_collide.gd
+++ spray_particle_collide.gd
@@ -16,7 +16,7 @@ func _on_br_particle_area_area_entered(area: Area2D) -> void:
 	#get_node(".").set_deferred("disabled", true);
 	
 	# Resets the particles to their starting position
-	get_node("../../../../../Area2D/AmppsBetter/UR_particle_spawner").reset(4000, 2600);
+	get_node("../../../../../Area2D/AmppsBetter/BR_particle_spawner").reset(5400, 3000);
 	#pass
 
 func _on_ur_particle_area_area_entered(area: Area2D) -> void:
@@ -24,7 +24,7 @@ func _on_ur_particle_area_area_entered(area: Area2D) -> void:
 	#get_node(".").set_deferred("disabled", true);
 	
 	# Resets the particles to their starting position
-	get_node("../../../../../Area2D/AmppsBetter/UR_particle_spawner").reset(4000, -2600);
+	get_node("../../../../../Area2D/AmppsBetter/UR_particle_spawner").reset(5400, -3200);
 	#pass
 
 
@@ -33,7 +33,7 @@ func _on_bl_particle_area_area_entered(area: Area2D) -> void:
 	#get_node(".").set_deferred("disabled", true);
 	
 	# Resets the particles to their starting position
-	get_node("../../../../../Area2D/AmppsBetter/BL_particle_spawner").reset(-4000, -2600);
+	get_node("../../../../../Area2D/AmppsBetter/BL_particle_spawner").reset(-5300, 3000);
 
 
 func _on_ul_particle_area_area_entered(area: Area2D) -> void:
@@ -41,18 +41,18 @@ func _on_ul_particle_area_area_entered(area: Area2D) -> void:
 	#get_node(".").set_deferred("disabled", true);
 	
 	# Resets the particles to their starting position
-	get_node("../../../../../Area2D/AmppsBetter/UL_particle_spawner").reset(-4000, 2600);
+	get_node("../../../../../Area2D/AmppsBetter/UL_particle_spawner").reset(-5300, -3200);
 	
 func _on_um_particle_area_area_entered(area: Area2D) -> void:
 	#get_node("../../../../../Area2D/AmppsBetter/UM_particle_spawner").queue_free();
 	#get_node(".").set_deferred("disabled", true);
 	
 	# Resets the particles to their starting position
-	get_node("../../../../../Area2D/AmppsBetter/UM_particle_spawner").reset(-2000, 2600);
+	get_node("../../../../../Area2D/AmppsBetter/UM_particle_spawner").reset(0, -3200);
 
 func _on_bm_particle_area_area_entered(area: Area2D) -> void:
 	#get_node("../../../../../Area2D/AmppsBetter/BM_particle_spawner").queue_free();
 	#get_node(".").set_deferred("disabled", true);
 	
 	# Resets the particles to their starting position
-	get_node("../../../../../Area2D/AmppsBetter/BM_particle_spawner").reset(2000, 2600);
+	get_node("../../../../../Area2D/AmppsBetter/BM_particle_spawner").reset(0, 3100);
